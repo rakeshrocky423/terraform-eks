75b8712b4a3e85abd8a462882e24acd2bd5af11a
rakeshrocky423 <162771496+rakeshrocky423@users.noreply.github.com>
2024-08-04 17:32:04 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -40,16 +40,7 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "iam:CreateRole",
-      "Resource": "*"
-    }
-  ]
-}
+
 
   }
 }
